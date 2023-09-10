375d6abaa0f59339d422b79d189b67e791adc073
Ivan <cortes.ivan353@gmail.com>
2023-09-09 20:55:08 -0600

    Improve images size for web

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { useRef, lazy, Suspense } from 'react'
 import * as styles from '../styles/index.module.scss'
 import { LanguageProvider } from '../components/languageContext'
